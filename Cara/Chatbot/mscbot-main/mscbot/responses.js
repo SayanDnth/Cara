f41527e87c6e0f62e41ce4af99952a2283a84d6c
Sayan Debnath <121996460+SayanDnth@users.noreply.github.com>
2023-05-13 15:01:12 +0530

    Add files via upload

---
+++ Cara/Chatbot/mscbot-main/mscbot/responses.js
@@ -0,0 +1,44 @@
+function getBotResponse(input) {
+    //rock paper scissors
+    // if (input == "rock") {
+    //     return "paper";
+    // } else if (input == "paper") {
+    //     return "scissors";
+    // } else if (input == "scissors") {
+    //     return "rock";
+    // }
+
+    // Simple responses
+    if (input == "hello") {
+        return "Hello there!";
+    } else if (input == "goodbye") {
+        return "Talk to you later!";
+    } else {
+        return "Try asking something else!";
+    }
+
+    //query 
+    if (input == "hello") {
+        return "Hello there!";
+    } else if (input == "goodbye") {
+        return "Talk to you later!";
+    } else {
+        return "Try asking something else!";
+    }
+
+    if (input == "hello") {
+        return "Hello there!";
+    } else if (input == "goodbye") {
+        return "Talk to you later!";
+    } else {
+        return "Try asking something else!";
+    }
+
+    if (input == "hello") {
+        return "Hello there!";
+    } else if (input == "goodbye") {
+        return "Talk to you later!";
+    } else {
+        return "Try asking something else!";
+    }
+}
